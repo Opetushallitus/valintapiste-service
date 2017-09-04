aaaa0ffc0d47c091b07dff36e3ae54a713b4583f
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-09-04 15:18:34 +0300

    clojure http lib added

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
  (defproject valintapiste-service "0.1.0-SNAPSHOT"
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
+                  [clj-http "3.7.0"]
                   [metosin/compojure-api "1.1.11"]
                   [ring/ring-jetty-adapter "1.6.2"]
                   [com.novemberain/monger "3.1.0"]]
