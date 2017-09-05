8a33d6e58e9870fdaf98566387964789002144f9
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-09-05 13:58:46 +0300

    cache test postgres under target/postgres

--- test/valintapiste_service/testpostgres.clj
+++ test/valintapiste_service/testpostgres.clj
@@ -1,8 +1,12 @@
 (ns valintapiste-service.testpostgres)
 
 (defn -main [] 
-    (let [postgres (new ru.yandex.qatools.embed.postgresql.EmbeddedPostgres)]
+    (let [postgresBinaryPath (. java.nio.file.Paths get "target" (into-array ["postgres"]))
+          cachedPostgres (. ru.yandex.qatools.embed.postgresql.EmbeddedPostgres cachedRuntimeConfig postgresBinaryPath)
+          additionalParams (. java.util.Collections emptyList)
+          postgres (new ru.yandex.qatools.embed.postgresql.EmbeddedPostgres)]
         (.addShutdownHook (Runtime/getRuntime) (Thread. #(. postgres (stop))))
-        (. postgres (start "localhost" 5432 "test" "test" "test"))
+        (. postgres (start cachedPostgres "localhost" 5432 "test" "test" "test" additionalParams))
+        (prn "Postgres started at localhost:5432/test! Username is test and password is test")
         (Thread/sleep 86400000)))
         
