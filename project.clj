383c4e1b917dbe8f0b5b2f75d7b14046455b1043
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-09-05 12:20:05 +0300

    typo in deps fixed

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                   [clj-http "3.7.0"]
                   [metosin/compojure-api "1.1.11"]
                   [ring/ring-jetty-adapter "1.6.2"]
-                  [com.novemberain/monger "3.1.0"]]
+                  [com.novemberain/monger "3.1.0"]
                   [ragtime "0.7.1"]]
    :ring {:handler valintapiste-service.handler/app}
    :uberjar-name "server.jar"
