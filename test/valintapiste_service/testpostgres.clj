81e80eb1ed0f00663669bd7d60035f2ed5c2ab92
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-09-05 13:27:46 +0300

    keep test postgres alive 24hours

--- test/valintapiste_service/testpostgres.clj
+++ test/valintapiste_service/testpostgres.clj
@@ -4,5 +4,5 @@
     (let [postgres (new ru.yandex.qatools.embed.postgresql.EmbeddedPostgres)]
         (.addShutdownHook (Runtime/getRuntime) (Thread. #(. postgres (stop))))
         (. postgres (start "localhost" 5432 "test" "test" "test"))
-        (Thread/sleep 10000)))
+        (Thread/sleep 86400000)))
         
