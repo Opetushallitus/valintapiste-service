daa38867dd6edffa56cccb348184413ab6eb1c3b
Tommi Helander <tommi.helander@gofore.com>
2017-09-06 12:46:52 +0300

    Add row function to convert row to pistetieto

--- resources/queries.sql
+++ resources/queries.sql
@@ -1,8 +1,9 @@
 -- name: find-valintapisteet-for-hakemus
+-- row-fn: to-pistetieto
 -- Returns valintapisteet for single hakemusoid
-select * from valintapiste where hakemus_oid = :hakemus-oid
+select tunnus, arvo from valintapiste where hakemus_oid = :hakemus-oid
 
 -- name: find-valintapisteet-for-hakemukset
 -- Returns valintapisteet for multiple hakemusoids
-select * from valintapiste where hakemus_oid in (:hakemus-oids)
+select tunnus, arvo from valintapiste where hakemus_oid in (:hakemus-oids)
 
--- src/valintapiste_service/pistetiedot.clj
+++ src/valintapiste_service/pistetiedot.clj
@@ -1,13 +1,16 @@
 (ns valintapiste-service.pistetiedot
   (:require [jeesql.core :refer [defqueries]]))
 
+(defn to-pistetieto [row]
+  {(:tunnus row) (:arvo row)})
+
 (defqueries "queries.sql")
 
 (defn fetch-hakemuksen-pistetiedot 
     "Returns pistetiedot for hakemus"
     [connection hakuOID hakemusOID]
-    (find-valintapisteet-for-hakemus connection {:hakemus-oid hakemusOID})
-    {:hakemusOID hakemusOID :pisteet {}})
+    (let [pistetiedot (find-valintapisteet-for-hakemus connection {:hakemus-oid hakemusOID})]
+      {:hakemusOID hakemusOID :pisteet pistetiedot}))
 
 (defn fetch-hakukohteen-pistetiedot 
   "Returns pistetiedot for hakukohde"
