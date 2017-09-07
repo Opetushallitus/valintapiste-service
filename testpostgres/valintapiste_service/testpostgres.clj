17ee7f3810faa73ff31c156dade199a4cca464c1
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-09-07 10:49:24 +0300

    embedded postgres now using same config as local 'lein run' and lein tests

--- testpostgres/valintapiste_service/testpostgres.clj
+++ testpostgres/valintapiste_service/testpostgres.clj
@@ -1,12 +1,21 @@
-(ns valintapiste-service.testpostgres)
+(ns valintapiste-service.testpostgres
+  (:require [valintapiste-service.config :as c]
+            [clj-log4j2.core :as log]))
+
 
 (defn -main [] 
-    (let [postgresBinaryPath (. java.nio.file.Paths get "target" (into-array ["postgres"]))
+    (let [config (c/readConfigurationFile)
+          servername (-> config :db :servername)
+          username (-> config :db :username)
+          password (-> config :db :password)
+          databasename (-> config :db :databasename)
+          port (-> config :db :port)
+          postgresBinaryPath (. java.nio.file.Paths get "target" (into-array ["postgres"]))
           cachedPostgres (. ru.yandex.qatools.embed.postgresql.EmbeddedPostgres cachedRuntimeConfig postgresBinaryPath)
           additionalParams (. java.util.Collections emptyList)
           postgres (new ru.yandex.qatools.embed.postgresql.EmbeddedPostgres)]
         (.addShutdownHook (Runtime/getRuntime) (Thread. #(. postgres (stop))))
-        (. postgres (start cachedPostgres "localhost" 5432 "test" "test" "test" additionalParams))
-        (prn "Postgres started at localhost:5432/test! Username is test and password is test")
+        (. postgres (start cachedPostgres servername port databasename username password additionalParams))
+        (log/info "Postgres started at {}:{}/{}! Username is {} and password is {}" servername port databasename username password)
         (Thread/sleep 86400000)))
         
