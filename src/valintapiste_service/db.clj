7cb0e5f43d86266fbea576603db074af9765ebf5
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-09-07 10:52:49 +0300

    code cleanup

--- src/valintapiste_service/db.clj
+++ src/valintapiste_service/db.clj
@@ -1,7 +1,9 @@
 (ns valintapiste-service.db)
 
+(def PATHS_TO_MIGRATIONS (into-array java.lang.String ["migrations"]))
+
 (defn migrate [datasource]
   (doto (new org.flywaydb.core.Flyway)
     (.setDataSource datasource)
-    (.setLocations (into-array java.lang.String ["migrations"]))
+    (.setLocations PATHS_TO_MIGRATIONS)
     (.migrate)))
