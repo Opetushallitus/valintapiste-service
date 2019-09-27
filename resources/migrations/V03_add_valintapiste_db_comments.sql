252e1befc29ea027e78e133ee2a8ee2e2aed829b
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-09-27 10:11:45 +0300

    OY-316 : Added db comments to migration.

---
+++ resources/migrations/V03_add_valintapiste_db_comments.sql
@@ -0,0 +1,9 @@
+comment on database valintapisteservice is 'Tietokanta hakijan valintakoetiedoille';
+comment on table valintapiste is 'Hakijan valintakokeiden osallistumis- ja pistetiedot'
+comment on column valintapiste.hakemus_oid is 'Hakemuksen OID';
+comment on column valintapiste.tunniste is 'Valintakokeen tunniste (syötettävä arvo)';
+comment on column valintapiste.arvo is 'Valintakokeen pisteet';
+comment on column valintapiste.osallistuminen is 'Valintakokeen osallistumistieto';
+comment on column valintapiste.tallettaja is 'Tallentajan OID';
+comment on column valintapiste.system_time is 'Tallennuksen aikaleima';
+comment on column valintapiste.transaction_id is 'Tallennuksen transaktio-id';
