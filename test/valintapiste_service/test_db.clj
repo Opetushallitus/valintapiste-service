af329e33c68d2b5ae7eee2c22b1319528c1e1b0c
August Kilponen <august.kilponen@gmail.com>
2024-11-06 17:40:20 +0200

    Päivitetty kirjastoja

--- test/valintapiste_service/test_db.clj
+++ test/valintapiste_service/test_db.clj
@@ -1,19 +1,19 @@
 (ns valintapiste-service.test-db
-    (require [clojure.java.jdbc :as j]))
+    (:require [clojure.java.jdbc :as j]))
 
 (defn pg-uri
     [config]
     {:connection-timeout 2500
-     :connection-uri (str "jdbc:postgresql://" 
-                            (-> config :db :servername) 
-                                ":" (-> config :db :port) 
-                                "/" (-> config :db :databasename) 
-                                "?user=" (-> config :db :username) 
+     :connection-uri (str "jdbc:postgresql://"
+                            (-> config :db :servername)
+                                ":" (-> config :db :port)
+                                "/" (-> config :db :databasename)
+                                "?user=" (-> config :db :username)
                                 "&password=" (-> config :db :password))})
 
-(defn testConnection 
+(defn testConnection
     "Test if connection works"
     [config]
-    (if (try 
+    (if (try
         (j/query (pg-uri config) ["SELECT 1"])
         (catch Exception e false)) {:connectionWorks true} {:connectionWorks false} ))
